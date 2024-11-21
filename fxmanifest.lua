8b134ce846c4c3eba17ef034eaadb863abdd31ce
Andrew <10902965+AndrewR3K@users.noreply.github.com>
2024-11-20 20:26:42 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "adamant"
 games { "rdr3" }
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 "yes"
-author "BCC Team"
+author "BCC Scripts @iseeyoucopy"
 
 shared_scripts {
     'config.lua',
